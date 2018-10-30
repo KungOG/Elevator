f39a53531cf280a563070339e27cabf6d825d0e1
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2018-10-30 21:47:06 +0100

    First commit, added just basic, still in progress

---
+++ main.js
@@ -0,0 +1,17 @@
+var floors = document.getElementById("floors");
+var floor = document.getElementsByClassName("floor")
+var elevator = 0;
+
+addEventListener('click', function() {
+    
+});
+
+
+
+
+
+aiuhfsabg === 4
+
+suighshg === 4
+
+sgopsgo === 3
