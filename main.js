11e5bece52f1dfd933b8505a48683651cbc64b3a
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2018-11-06 15:01:05 +0100

    added loop and button pushed

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-var floors = document.getElementById("floors");
-var floor = document.getElementsByClassName("floor")
-var elevator = 0;
+var floors = document.getElementById("floors")
+var floor = document.getElementsByClassName("floor");
+var currentFloor = 4;
+var buttonPushed;
+var lastFloor;
+var destination;
 
-addEventListener('click', function() {
+
+for(var i = 0; i < floor.length; i++){
+floor[i].addEventListener('click', function(e) {
     
-});
+    buttonPushed = e.target.id;
+    Elevator(buttonPushed);
+    console.log(destination)
+    if (destination != currentFloor) {
+        Object.assign(floor[destination].style,{backgroundColor:"yellow"});
+        }
+       
+  });
+}
 
-
-
-
-
-aiuhfsabg === 4
-
-suighshg === 4
-
-sgopsgo === 3
+function Elevator(buttonPushed) {
+    if(buttonPushed == 5) {
+        destination = 0;
+    } else if (buttonPushed == 4) {
+        destination = 1;
+    } else if (buttonPushed == 3) {
+        destination = 2;
+    } else if (buttonPushed == 2) {
+        destination = 3;
+    } else {
+        destination = 4;
+    }
+}
